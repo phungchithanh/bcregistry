875342f2cd4df8f21d7895f31f18ea50c4bf5bc1
phungchithanh <phungchithanh@gmail.com>
2025-05-07 11:23:57 -0600

    Add display link for product fees
    Add display link for product fees

---
+++ app/components/content/ProductFeesLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    :href="productFeesURL"
+    target="_blank"
+    class="prose">
+    Learn more about applicable product fees
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { useRuntimeConfig } from '#app'
+
+const runtimeConfig = useRuntimeConfig()
+
+const productFeesURL = `${runtimeConfig.public.productFeesURL}`
+</script>
+
+<style scoped>
+.prose{
+  color: var(--color-bcGovColor-markBlue);
+  text-decoration: underline;
+  font-weight: 900;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -157,7 +157,8 @@ export default defineNuxtConfig({
       pprDashboard: `${process.env.NUXT_PPR_URL}/dashboard`,
       strrURL: process.env.NUXT_STRR_URL,
       createAccountNoAuthURL: `${process.env.NUXT_AUTH_WEB_URL}choose-authentication-method`,
-      ldClientId: process.env.NUXT_LD_CLIENT_ID || ''
+      ldClientId: process.env.NUXT_LD_CLIENT_ID || '',
+      productFeesURL: `${process.env.NUXT_BASE_URL}product-fees`
     }
   }
 })
